7fe027b27502f9ef70cf80d483931334eeb2e041
mochivi <victormochi1@gmail.com>
2024-10-04 16:21:07 -0300

    database setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"gopher_social/internal/db"
 	"gopher_social/internal/env"
 	"gopher_social/internal/store"
 	"log"
@@ -19,10 +20,29 @@ func main() {
 	// Create config
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:adminpassword@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
+	// Create database
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+	log.Println("database connection pool estabilished")
+
 	// Create store
-	store := store.NewStorage(nil)
+	store := store.NewStorage(db)
 
 	// Create application
 	app := &application{
